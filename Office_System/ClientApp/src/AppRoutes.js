9396e2804ccd693197a86dfd8f5f7e328dbf8fe2
e_szeliga <eszeliga@student.agh.edu.pl>
2023-03-19 18:25:48 +0100

    Add navigation to RoomReservation

--- Office_System/ClientApp/src/AppRoutes.js
+++ Office_System/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { RoomReservationUI } from "./components/RoomReservationUI"
 
 const AppRoutes = [
   {
@@ -14,6 +15,10 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+    },
+  {
+     path: '/room-reservation',
+     element: <RoomReservationUI />
   }
 ];
 
